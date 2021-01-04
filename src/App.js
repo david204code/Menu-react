ff5ca58103515ed8251b4b99e73c6f13917ebd9c
David Wu <david204code@gmail.com>
2021-01-04 18:14:38 +0000

    Set up the state value and rendering the two component Categories & Menu

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+
+  // state value
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);;
+
+  return(
+    <>
+      <main>
+        <section className ="menue">
+
+          <div className ="title">
+            <h2>Our Menu</h2>
+            <div className ="underline"></div>
+          </div>
+
+          <Categories />
+          <Menu />
+          
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
