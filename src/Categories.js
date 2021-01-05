1ae5aff9ef9c08c9b16ee1a6b7c8f0e99041b29e
David Wu <david204code@gmail.com>
2021-01-05 19:48:39 +0000

    Completed map through the categories and displays it dynamically

--- src/Categories.js
+++ src/Categories.js
@@ -25,16 +25,17 @@ const Categories = ({ categories, filterItems}) => {
   return(
     <>
       <div className ="btn-container">
-        <button className ="filter-btn" 
-          onClick ={() => filterItems('all')}
-        >
-          All
-        </button>
-        <button className ="filter-btn"
-          onClick ={() => filterItems('breakfast')}
-        >
-          Breakfast
-        </button>
+        {categories.map((category, index) => {
+          return (
+            <button type ="button" 
+              className ="filter-btn" 
+              key ={index}
+              onClick ={() => filterItems(category)}
+            >
+              {category}
+            </button>
+          )
+        })}
       </div>
     </>
   );
